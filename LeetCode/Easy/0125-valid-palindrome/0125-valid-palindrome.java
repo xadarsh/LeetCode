338daf1d8bf35430ea6cc6f261cc7417d38d448c
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-08-31 16:11:55 +0530

    Time: 5 ms (48.43%), Space: 48.9 MB (5.67%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        StringBuilder res= new StringBuilder();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(c>='a' && c <='z' || c>='0' && c<='9')
+            {
+                res.append(c);
+            }
+        }
+
+        return checkPalindrome(res.toString(),0);
+    }
+
+    public static boolean checkPalindrome(String res, int index)
+    {
+        if(index>=res.length()/2)
+        {
+            return true;
+        }
+        else if(res.charAt(index)!=res.charAt(res.length()-1-index) )
+        {
+            return false;
+        }
+        return checkPalindrome(res,index+1);
+    }
+}
