0109606c78d12289d5dd091b94c97ed35fb2530a
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-09-04 22:20:03 +0530

    Time:2 ms (79.7%), Difficulty:Easy - Date:09-04-2025

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int count=0;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]!=0){
-            nums[count++]=nums[i];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp=nums[count];
+                nums[count]=nums[i];
+                nums[i]=temp;
+                count++;
+            }
         }
-       }
-       for(int i=count;i<nums.length;i++){
-        nums[i]=0;
-       }
 
     }
 }
