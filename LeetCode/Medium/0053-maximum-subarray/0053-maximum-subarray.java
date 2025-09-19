11598043018939ccf8c4004b2ebf99a66de202e8
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-09-19 09:39:04 +0530

    Time:1 ms (99.48%), Difficulty:Medium - Date:09-19-2025

---
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int len=nums.length;
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int i:nums)
+        {
+            sum+=i;
+            if(max<sum)
+                max=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return max;
+    }
+}
