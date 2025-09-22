6425a3411c0e99d4b37659bd02c39226db378c3e
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-09-22 11:23:20 +0530

    Time:N/A (0%), Difficulty:Medium - Date:09-22-2025

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,13 +5,26 @@ class Solution {
         //int[] colArray=new int[colLen];
         //int[] rowArray=new int[rowLen];
 
-        //boolean isRow=false;
+        boolean isRow=false;
         boolean isCol=false;
         
+        //checking the rows
         for(int i=0;i<rowLen;i++)
         {
             if(matrix[i][0]==0)
+                isRow=true;
+        }
+
+        //check the column
+        for(int j=0;j<colLen;j++)
+        {
+            if(matrix[0][j]==0)
                 isCol=true;
+        }
+
+        //checking other rows and columns
+        for(int i=1;i<rowLen;i++)
+        {
             for(int j=1;j<colLen;j++)
             {
                 
@@ -19,10 +32,11 @@ class Solution {
                 {
                     matrix[0][j]=0;
                     matrix[i][0]=0;
-                    //isRow=true;
                 }
             }
         }
+
+        //setting other rows and columns 0
         for(int i=rowLen-1;i>=0;i--)
         {
             for(int j=colLen-1;j>=1;j--)
@@ -30,7 +44,19 @@ class Solution {
                 if(matrix[0][j]==0 || matrix[i][0]==0)
                     matrix[i][j]=0;
             }
-            if(isCol)
+        }
+
+        //setting rows to 0 based on condition
+        if(isRow)
+        {
+            for(int j=0;j<colLen;j++)
+                matrix[0][j]=0;
+        }
+
+        //setting columns to 0 based on condition
+        if(isCol)
+        {
+            for(int i=0;i<rowLen;i++)
                 matrix[i][0]=0;
         }
     }
