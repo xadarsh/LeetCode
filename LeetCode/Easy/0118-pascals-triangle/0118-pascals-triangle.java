6fefd8dc2d55f0f5257ac36bea2e1075149788c5
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-09-27 15:09:19 +0530

    Time:1 ms (87.16%), Difficulty:Easy - Date:09-27-2025

---
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> list= new ArrayList<>();
+
+        for(int i=0;i<numRows;i++)
+        {
+            list.add(new ArrayList<>());
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0 || j==i)
+                {
+                    list.get(i).add(1);
+                }
+                else
+                {
+                    list.get(i).add(list.get(i-1).get(j-1)+list.get(i-1).get(j));
+                }
+            }
+        }
+
+        return list;
+
+    }
+}
