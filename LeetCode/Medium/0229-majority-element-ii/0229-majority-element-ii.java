c2b9620b22435b3e206c431c78e285ed999822b3
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-09-30 21:22:22 +0530

    Time:3 ms (74.67%), Difficulty:Medium - Date:09-30-2025

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,48 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> list=new ArrayList<>();
-        int count=1;
+        int cnt1=0;
+        int cnt2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
         int len=nums.length;
         int limit=len/3;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<len;i++)
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         
-        for(int i: hm.keySet())
+        for(int i=0;i<len;i++)
         {
-            if(hm.get(i)>limit)
-                list.add(i);
+            if(cnt1==0 && ele2!=nums[i])
+            {
+                cnt1=1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && ele1!=nums[i])
+            {
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(ele1==nums[i])
+                cnt1++;
+            else if(ele2==nums[i])
+                cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
         }
+        cnt1=0;
+        cnt2=0;
+        for(int i:nums)
+        {
+            if(i==ele1)
+                cnt1++;
+            else if(i==ele2)
+                cnt2++;
+        }
+        if(cnt1>limit)
+            list.add(ele1);
+        if(cnt2>limit)
+            list.add(ele2);
         return list;
     }
 }
