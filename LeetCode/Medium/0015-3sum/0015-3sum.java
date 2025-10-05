bdfe1817c4c8df10a026bed515ac89c8b81b561e
Adarsh Maurya <100351418+xadarsh@users.noreply.github.com>
2025-10-05 22:21:46 +0530

    Time:31 ms (56.58%), Difficulty:Medium - Date:10-05-2025

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,28 +1,39 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Sort first for two-pointer logic
         List<List<Integer>> res = new ArrayList<>();
-        int len = nums.length;
+        int n = nums.length;
 
-        for (int i = 0; i < len; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            int j = i + 1, k = len - 1;
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    int tempJ = nums[j], tempK = nums[k];
-                    while (j < k && nums[j] == tempJ) j++;
-                    while (j < k && nums[k] == tempK) k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate values for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++; // Need a larger sum
+                } 
+                else {
+                    right--; // Need a smaller sum
                 }
             }
         }
+
         return res;
     }
 }
